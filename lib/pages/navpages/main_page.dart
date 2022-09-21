d2ff52fe4625f1d7613efb256d1f127ccb3b8d28
1Mathias <mathiasabdisa@gmail.com>
2022-09-21 08:51:39 +0300

    Main Page update

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit/pages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/my_page.dart';
+import 'package:flutter_cubit/pages/navpages/search_page.dart';
+
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -8,8 +13,55 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black87,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            label: "Account",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: "Menu",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Setting",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Person",
+          ),
+        ],
+      ),
+    );
   }
 }
