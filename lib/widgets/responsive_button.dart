1e5ab884dbd90a20c571b34233f2385d8b0c6d17
1Mathias <mathiasabdisa@gmail.com>
2022-09-28 18:11:16 +0300

    main page update

--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
 
 class ResponsiveButton extends StatelessWidget {
   bool? isResponsive;
@@ -7,25 +8,35 @@ class ResponsiveButton extends StatelessWidget {
   ResponsiveButton({
     Key? key,
     this.isResponsive = false,
-    this.width,
+    this.width = 120,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.mainColor,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "img/button-one.png",
-          )
-        ],
+    return Flexible(
+      child: Container(
+        width: isResponsive == true ? double.maxFinite : width,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.mainColor,
+        ),
+        child: Row(
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.spaceBetween,
+          children: [
+            isResponsive == true
+                ? AppText(
+                    text: "Book Trip Now",
+                    color: Colors.white,
+                  )
+                : Container(),
+            Image.asset(
+              "img/button-one.png",
+            )
+          ],
+        ),
       ),
     );
   }
